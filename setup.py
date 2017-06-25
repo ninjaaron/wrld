904b082dd712ae27f8f06ee361697040af3c2c73
Aaron Christianson <ninjaaron@gmail.com>
2017-06-25 20:26:23 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import fastentrypoints
 
 setup(
     name='wrld',
-    version='0.6',
+    version='0.7',
     author='Aaron Christianson',
     license='BSD',
     author_email='ninjaaron@gmail.com',
